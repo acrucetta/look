05504aa0b0901041e50a30500984dd3ccd517bde
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2023-05-01 15:28:25 -0500

    Add CLI search functionality to search indexed documents

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,46 @@
+use clap::{App, Arg};
+use indexer::{search, Index};
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("CLI Search")
+        .version("0.1.0")
+        .author("Your Name <your.email@example.com>")
+        .about("Searches in the indexed documents")
+        .arg(
+            Arg::new("query")
+                .about("The query to search for")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::new("index_path")
+                .short('i')
+                .long("index")
+                .value_name("INDEX_PATH")
+                .about("The path to the index.json file")
+                .takes_value(true)
+                .default_value("index.json"),
+        )
+        .get_matches();
+
+    let query = matches.value_of("query").unwrap();
+    let index_path = matches.value_of("index_path").unwrap();
+
+    let index = load_index_from_json_file(Path::new(index_path)).expect("Failed to load index");
+    let results = search(query, &index).expect("Search failed");
+
+    for result in results {
+        println!("{}", result.document_path);
+    }
+}
+
+fn load_index_from_json_file(index_path: &Path) -> std::io::Result<Index> {
+    let mut file = File::open(index_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let index: Index = serde_json::from_str(&contents)?;
+    Ok(index)
 }
--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -3,10 +3,11 @@ mod indexer;
 mod search_query;
 use indexer::file_processing::process_directory;
 use indexer::Index;
+use std::env;
 use std::path::Path;
 
 fn main() {
-    let dir_path = "personal_data";
+    let dir_path = env::var("DIR_PATH").expect("DIR_PATH not set");
     let mut index = Index::new();
 
     // Update your `process_file` function to accept a `&mut HashMap<String, Vec<String>>` argument and pass it to `store_processed_text_in_index`
@@ -22,27 +23,6 @@ fn main() {
     let output_path = Path::new("index.json");
     match index.save_index_to_json_file(output_path) {
         Ok(_) => println!("Index saved to {}.", output_path.to_str().unwrap()),
-        Err(e) => println!("Error occurred: {}", e),
-    }
-
-    // Get the search query from the user
-    println!("Enter your search query:");
-    let mut query = String::new();
-    std::io::stdin()
-        .read_line(&mut query)
-        .expect("Failed to read line");
-
-    // Search the index for the query
-    let search_results = search_query::search(&query, &index);
-
-    // Print the search results
-    println!("Search results:");
-    match search_results {
-        Ok(results) => {
-            for result in results {
-                println!("{}", result);
-            }
-        }
         Err(e) => println!("Error occurred: {}", e),
     }
 }
