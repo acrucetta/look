8d8745fc3c8b66b13dfceba5e4e7cc0ff3857dd0
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2023-05-01 22:10:13 -0500

    Add `dotenv` dependency to CLI and update `reindex` function to use a hardcoded index path and process a directory, instead of using an environment variable and the `data_ingestion` module

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,10 +1,9 @@
 use clap::{arg, command, Command};
+use indexer::index_builder::file_processing::process_directory;
 use indexer::index_builder::Index;
 use indexer::search_query;
 use indexer::search_query::SearchResult;
 use std::env;
-use std::fs::File;
-use std::io::Read;
 use std::path::Path;
 
 fn main() {
@@ -23,17 +22,19 @@ fn main() {
         )
         .get_matches();
 
-    let index_path = env::var("INDEX_PATH").expect("INDEX_PATH not set");
-    let index = load_index_from_json_file(Path::new(&index_path)).expect("Failed to load index");
+    let index_path = "index.json";
+    let index =
+        Index::load_index_from_json_file(Path::new(&index_path)).expect("Failed to load index");
 
     match matches.subcommand() {
         Some(("fd", matches)) => {
             let query = matches.get_one::<String>("QUERY").unwrap();
             search(query, index);
         }
-        Some(("reindex", matches)) => {
+        Some(("reindex", _matches)) => {
             reindex();
         }
+        _ => unreachable!(),
     }
 }
 
@@ -53,35 +54,104 @@ fn search(query: &String, index: Index) -> Result<Vec<SearchResult>, Box<dyn std
     println!("Search results:");
     match search_results {
         Ok(results) => {
-            for result in results {
+            for result in &results {
                 println!("{}", result);
             }
+            Ok(results)
         }
-        Err(e) => println!("Error occurred: {}", e),
+        Err(e) => Err(e),
     }
-    search_results
 }
 
 /// Re-index a directory
 ///
 /// This function will re-index a directory and save the index to the INDEX_PATH
 fn reindex() {
-    todo!()
+    let dir_path = "personal_data";
+    let mut index = Index::new();
+
+    // Update your `process_file` function to accept a `&mut HashMap<String, Vec<String>>` argument and pass it to `store_processed_text_in_index`
+    match process_directory(dir_path, &mut index) {
+        Ok(_) => println!("Processing completed."),
+        Err(e) => println!("Error occurred: {}", e),
+    }
+
+    // Calculate the IDF for each term
+    index.calculate_idf();
+
+    // Save the in-memory index to a JSON file
+    let output_path = Path::new("index.json");
+    match index.save_index_to_json_file(output_path) {
+        Ok(_) => println!("Index saved to {}.", output_path.to_str().unwrap()),
+        Err(e) => println!("Error occurred: {}", e),
+    }
 }
 
-/// Load an index from a JSON file
-///
-/// This function will load an index from a JSON file
-///
-/// # Arguments
-///  * `index_path` - The path to the JSON file
-///
-/// # Returns
-/// * `std::io::Result<Index>` - The index
-fn load_index_from_json_file(index_path: &Path) -> std::io::Result<Index> {
-    let mut file = File::open(index_path)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    let index: Index = serde_json::from_str(&contents)?;
-    Ok(index)
+#[cfg(test)]
+mod tests {
+    use std::{env, path::Path};
+
+    fn build_index_with_3_docs() -> super::Index {
+        use super::Index;
+        use indexer::index_builder::Document;
+
+        let mut index = Index::new();
+        let text = "This is a test sentence.";
+        let path = Path::new("test.txt");
+        let document = Document::new(path.to_str().unwrap().to_owned());
+        index.store_processed_text_in_index(&document, &text);
+
+        let text = "This is another test sentence.";
+        let path = Path::new("test2.txt");
+        let document = Document::new(path.to_str().unwrap().to_owned());
+        index.store_processed_text_in_index(&document, &text);
+
+        let text = "This is a third test sentence.";
+        let path = Path::new("test3.txt");
+        let document = Document::new(path.to_str().unwrap().to_owned());
+        index.store_processed_text_in_index(&document, &text);
+
+        // Save the index to a JSON file
+        index.calculate_idf();
+        index
+            .save_index_to_json_file(&Path::new("test_index.json"))
+            .unwrap();
+        index
+    }
+
+    #[test]
+    fn test_search() {
+        let index = build_index_with_3_docs();
+
+        let query = "test";
+        let results = super::search(&query.to_owned(), index).unwrap();
+
+        assert_eq!(results.len(), 3);
+    }
+
+    #[test]
+    fn test_reindex() {
+        // Set the current directory to the root of the project
+        let root_dir = env::current_dir().unwrap();
+
+        let mut index_path = root_dir.clone();
+        index_path.push("index.json");
+
+        let mut personal_data_path = root_dir.clone();
+        personal_data_path.push("personal_data");
+
+        // Delete the index file if it exists
+        if index_path.exists() {
+            std::fs::remove_file(index_path.clone()).unwrap();
+        }
+
+        // Re-index the directory
+        super::reindex();
+
+        // Check that the index file was created
+        assert!(index_path.exists());
+
+        // Delete the index file
+        std::fs::remove_file(index_path.clone()).unwrap();
+    }
 }
