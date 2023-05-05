f297f70bc878a6abeea62809a159c920ee1c66d3
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2023-05-05 09:18:36 -0500

    Refactor the `get_relative_path` function to take a string reference as input and use ANSI escape codes to create clickable links for file paths in the `format_cli_output` function

--- cli/src/formatter.rs
+++ cli/src/formatter.rs
@@ -23,33 +23,35 @@ const ENCODE_SET: &AsciiSet = &CONTROLS.add(b' ').add(b'#').add(b'?');
 /// The paths of the documents will be clickable, we will shorten them
 /// to 20 characters and add an ellipsis if they are longer than 20 characters
 /// they will also be blue ansi.
-pub(crate) fn format_cli_output(results: Vec<SearchResult>) -> String {
+pub fn format_cli_output(results: Vec<SearchResult>) -> String {
     let mut output = String::new();
-    output.push_str("Search results\n");
-    output.push_str("----------------\n");
     for result in results.iter().take(10) {
         let path = encode_path(Path::new(&result.document.path));
-        let relative_path = get_relative_path(path);
+        let relative_path = get_relative_path(&path);
         let formatted_path = Blue.bold().paint(relative_path).to_string();
         output.push_str(&format!("\n{} [{:.2}]\n", formatted_path, result.score))
     }
     output
 }
 
+/// Print the lines of code that match the query
+
 /// Format the path with respect to the current paths
-fn get_relative_path(path: String) -> String {
+fn get_relative_path(path: &str) -> String {
     // We want to print the relative_path with
     // respect to the current working directory
     let current_wd = env::current_dir().unwrap();
-    let full_path = Path::new(&path);
+    let full_path = Path::new(path);
     let relative_path = match full_path.strip_prefix(current_wd) {
         Ok(p) => p,
         Err(_) => full_path,
     };
-    let hyperlink = format!(
-        "\u{1b}[38;5;81;4mfile://{}\u{1b}[0m",
-        relative_path.display()
-    );
+    let file_url = format!("file://{}", path);
+    let alias = Blue
+        .bold()
+        .paint(relative_path.display().to_string())
+        .to_string();
+    let hyperlink = format!("\x1B]8;;{}\x1B\\{}\x1B]8;;\x1B\\", file_url, alias);
     hyperlink
 }
 
@@ -72,7 +74,7 @@ mod tests {
     #[test]
     fn test_relative_path() {
         let path = "/home/username/test.txt";
-        let relative_path = super::get_relative_path(path.to_string());
+        let relative_path = super::get_relative_path(path);
         assert_eq!(relative_path, "test.txt");
     }
 }
